2c0f30d0b360f3e5e3985b6d34bb9194819504d8
Mani Shukla <shuklamani999@gmail.com>
2021-07-26 22:09:18 +0530

    new11

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React, {Component} from "react";
 import "./App.css";
 import axios from "axios";
 import { useEffect, useState } from "react";
@@ -9,6 +10,7 @@ import TableContainer from "@material-ui/core/TableContainer";
 import TableHead from "@material-ui/core/TableHead";
 import TableRow from "@material-ui/core/TableRow";
 import Paper from "@material-ui/core/Paper";
+import { array } from "yargs";
 
 const StyledTableCell = withStyles((theme) => ({
   head: {
@@ -34,68 +36,85 @@ const useStyles = makeStyles({
   },
 });
 
-const App = () => {
-  const classes = useStyles();
-  const [product, setProduct] = useState([]);
-  const [search, setSearch] = useState("");
-  const [price, setPrice] = useState("");
-  const getProductData = async () => {
-    try {
-      const data = await axios.get(
-        "http://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline"
-        // "https://management.azure.com/{scope}/providers/Microsoft.CostManagement/query?api-version=2019-11-01"
-      );
-      console.log(data.data);
-      setProduct(data.data);
-    } catch (e) {
-      console.log(e);
+export class App extends Component{
+  constructor(props)
+  {
+    super(props);
+    this.state={
+      subscriptionID:'',
+      date:'',
+      resourceGroup:''
     }
-  };
+  }
+  changeHandler = (e) =>{
+    this.setState({[e.target.name]:e.target.value})
+  }
+// handleSubsId=(event)=>{
+//   this.setState({
+//     subsId:event.target.value
+//   });
+// }  
+// handleDate=(event)=>{
+//   this.setState({
+//     date:event.target.value
+//   });
+// }  
+// handleresourseGroup=(event)=>{
+//   this.setState({
+//     resourseGroup:event.target.value
+//   });
+// }  
+handleSubmit=(event)=>{
+  
+//  alert(this.state.subsId + this.state.resourseGroup )
+event.preventDefault()  
+console.log(this.state)
+let a={...this.state}
+a.resourceGroup=[a.resourceGroup]
+axios.post("http://localhost:3001/getCost",a)
+.then(response =>{
+  console.log(response.data.properties.rows);
+}
 
-  useEffect(() => {
-    getProductData();
-  }, []);
+)
+} 
+
+  render()
+  {
   return (
+    
     <div className="App">
-      <h1>Lets code tamil</h1>
+      
+      <h1>Tech Project</h1>
+      <form onSubmit={this.handleSubmit}>
       <input
         type="text"
-        placeholder="Search here"
-        onChange={(e) => {
-          setSearch(e.target.value);
-        }}
+        placeholder="Subscription ID"
+        value={this.state.subscriptionID}
+        name="subscriptionID"
+        onChange={this.changeHandler}
       />
       <input
-        type="text"
-        placeholder="Price Here"
-        onChange={(e) => {
-          setPrice(e.target.value);
-        }}
-      />
-      <input
-        type="date"
-        placeholder="date"
         
-      
+        type="text"
+        placeholder="resourceGroup"
+        value={this.state.resourceGroup}
+        name="resourceGroup"
+        onChange={this.changeHandler}
       />
+      <input
+        type="month"
+        placeholder="date"
+        value={this.state.date}
+        name="date"
+        onChange={this.changeHandler}
       
 
-      {/* {product
-        .filter((item) => {
-          if (search == "") {
-            return item;
-          } else if (item.name.toLowerCase().includes(search.toLowerCase())) {
-            return item;
-          }
-        })
-        .map((item) => {
-          return (
-            <p>
-              {item.name} - {item.price}
-            </p>
-          );
-        })} */}
+      />
+      <button>Submit</button>
+      </form>
 
+{/* 
       <TableContainer component={Paper}>
         <Table className={classes.table} aria-label="customized table">
           <TableHead>
@@ -132,9 +151,10 @@ const App = () => {
               })}
           </TableBody>
         </Table>
-      </TableContainer>
+      </TableContainer>  */}
     </div>
   );
+            }
 };
 
 export default App;
